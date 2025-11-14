e9328c29d2f33095e11edfddd212f6928ee5cdc6
Richard Lopes Strougo <art0066403@aluno.firjansenaisesi.com.br>
2025-11-14 15:58:17 -0300

    Adicionando Estado de Erro no Input

--- src/components/atoms/Input.jsx
+++ src/components/atoms/Input.jsx
@@ -7,18 +7,25 @@ Props:
 - type: O tipo de input (ex: 'text', 'password', 'email').
 */
 
-function Input({ type = 'text', placeholder }) {
+function Input({ type = 'text', placeholder, estado = 'padrao' }) {
     const [value, setValue] = useState('');
     const [isFocused, setIsFocused] = useState(false);
 
-    // Determina o estado no CSS com base no Foco e se Há texto
+    // Determina o estado no CSS se Há texto
     const hasValue = value.length > 0; // Verifica se há texto no input
-    const inputState = isFocused ? 'focado' : (hasValue ? 'digitado' : 'placeholder');
+   
+    // Determina as classes CSS com base no foco, valor e estado de erro
+    const stateClasses = `
+    ${styles.inputBase}
+    ${isFocused ? styles.focado : ''}
+    ${hasValue ? styles.digitado : styles.placeholder}
+    ${estado === 'erro' ? styles.erro : ''}
+    `;
 
     return (
         <input
         type={type} // Define o tipo de input
-        className={`${styles.inputBase} ${styles[inputState]}`} // Aplica classes CSS baseadas no estado
+        className={stateClasses} // Aplica classes CSS baseadas no estado
         placeholder={placeholder} // Define o placeholder
         value={value} // Define o valor do input
         onChange={(e) => setValue(e.target.value)} // Atualiza o valor do input ao digitar
